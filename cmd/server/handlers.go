21c1a12cea9c9c40a876924a1f27f725f8932b83
datnguyen210 <datnguyenftu210@gmail.com>
2024-08-19 16:57:41 +0700

    serve a static html file

---
+++ cmd/server/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+	   
+func productView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific product..."))
+}
+
+func productCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new product..."))
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,27 +7,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
-func productView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific product..."))
-}
-
-func productCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Create a new product..."))
-}
-
 func main() {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
@@ -41,11 +20,4 @@ func main() {
 	log.Printf("Starting server on %s", port)
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
-
-	// viper.SetConfigFile(".env")
-	// viper.ReadInConfig()
-	// port := viper.GetInt("PORT")
-
-	// r := routers.NewRouter()
-	// r.Run(fmt.Sprintf(":%d", port))
 }
