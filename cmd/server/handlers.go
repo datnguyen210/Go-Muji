4ec578bc8d67667b41c9ee42ef6bf847476fb40c
datnguyen210 <datnguyenftu210@gmail.com>
2024-08-21 17:33:06 +0700

    dependencies injection & error handling centralization

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -17,31 +18,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
-	
+
 	ts, err := template.ParseFiles(files...) // destructure
 	if err != nil {
-		log.Println(err.Error())
-		// return a general error message instead of letting 
-		// the users know more info to avoid attacker
-		http.Error(w, "Internal Server Error", 500) 
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
-func blogView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific blog..."))
+func (app *application) blogView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func blogCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) blogCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new blog..."))
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-
-	"github.com/spf13/viper"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-	viper.SetConfigFile(".env")
-	viper.ReadInConfig()
-	port := ":" + viper.GetString("PORT")
+	// catching the command line flag
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", app.home)
+	// Equivalent to: mux.Handle("/", http.HandlerFunc(home))
+	mux.HandleFunc("/blog/view", app.blogView)
+	mux.HandleFunc("/blog/create", app.blogCreate)
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/blog/view", blogView)
-	mux.HandleFunc("/blog/create", blogCreate)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
-	log.Printf("Starting server on %s", port)
-	err := http.ListenAndServe(port, mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
