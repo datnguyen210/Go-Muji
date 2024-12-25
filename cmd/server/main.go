fcb2fbc9d19840c28ad277f97f2d8dd7a4b49a4b
datnguyen210 <datnguyenftu210@gmail.com>
2024-12-25 14:12:56 +0700

    add TLS cert

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -66,7 +66,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
